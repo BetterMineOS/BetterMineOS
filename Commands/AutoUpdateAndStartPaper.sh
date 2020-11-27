0ad243cd59bb0a1452035995dfff2a092aafbeea
Wesley Duerksen <wesley.duerksen@wallawalla.edu>
2020-11-26 18:27:48 -0800

    updated the REGX and curl commands

--- Commands/AutoUpdateAndStartPaper.sh
+++ Commands/AutoUpdateAndStartPaper.sh
@@ -9,7 +9,7 @@ min_ram=6G
 max_ram=6G
 
 curl -o versions.txt $version_url
-latest=$(grep -oe '"latest":"[0-9]\+"' versions.txt | grep -oe '[0-9]\+')
+latest=$(grep -oe '"latest":[0-9]\+' versions.txt | grep -oe '[0-9]\+')
 rm versions.txt
 
 echo "Latest version of paper "$version": "$latest
@@ -21,7 +21,7 @@ then
     java -Xms$min_ram -Xmx$max_ram -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar paper-$latest.jar nogui
 else
     echo You are out of date, updating...
-    curl -JLO "https://papermc.io/api/v1/paper/"$version"/latest/download"
+    curl -JLo "paper-$latest.jar" "https://papermc.io/api/v1/paper/"$version"/latest/download"
 
     java -Xms$min_ram -Xmx$max_ram -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar paper-$latest.jar nogui
 fi
