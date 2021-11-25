c64c34f11f0a2c74acc138629697bef2fdfae20b
btw-ILTG <speterhess8@gmail.com>
2021-11-24 16:55:36 -0800

    Fixed helpless Wesless with the api (#1)
    * Fixed helpless Wesless with the api
    
    * fixed words
    
    Co-authored-by: Stefan Hess <speterhess@gmail.com>

--- Commands/AutoUpdateAndStartPaper.sh
+++ Commands/AutoUpdateAndStartPaper.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 
-version="1.16.4"
+version="1.17.1"
 
-version_url="https://papermc.io/api/v1/paper/"$version
-latest_url="https://papermc.io/api/v1/paper/"$version"/latest/download"
+version_url=https://papermc.io/api/v2/projects/paper/versions/$version
 
 min_ram=6G
 max_ram=6G
 
-curl -o versions.txt $version_url
-latest=$(grep -oe '"latest":[0-9]\+' versions.txt | grep -oe '[0-9]\+')
-rm versions.txt
+builds_string=$(curl -X GET "$version_url" -H  "accept: application/json")
 
-echo "Latest version of paper "$version": "$latest
+latest=$(echo $builds_string | rev | cut -d, -f1 | rev | cut -d] -f1)
 
-if [ "$(ls | grep $latest)" == "paper-$latest.jar" ]; then
-    echo You are up to date, starting paper-$latest.jar
+download_url="$version_url/builds/$latest/downloads/paper-$version-$latest.jar"
 
-    java -Xms$min_ram -Xmx$max_ram -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar paper-$latest.jar nogui
+echo "Latest version of paper $version: "$latest
+
+if [ "$(ls | grep $latest)" == "paper-$version-$latest.jar" ]; then
+    echo You are up to date, starting paper-$version-$latest.jar
+
+    java -Xms$min_ram -Xmx$max_ram -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar paper-$version-$latest.jar nogui
 else
     echo You are out of date, updating...
-    curl -JLo "paper-$latest.jar" $latest_url
+    curl -OX GET $download_url -H "accept: application/json"
 
-    java -Xms$min_ram -Xmx$max_ram -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar paper-$latest.jar nogui
+    java -Xms$min_ram -Xmx$max_ram -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar paper-$version-$latest.jar nogui
 fi
